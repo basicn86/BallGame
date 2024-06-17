a1316bcf8a22531c0d3b5278e8a1067cdab2f99b
Nedim Basic <basicn86@gmail.com>
2024-06-16 23:19:09 -0400

    Added laser projectile

---
+++ weapons/LaserPistol/LaserProjectile.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class LaserProjectile : Area3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		 
+	}
+}
