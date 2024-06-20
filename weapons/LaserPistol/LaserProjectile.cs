6b61a91c7ef94aa83b7979854c0b764a1f4cc800
Nedim Basic <basicn86@gmail.com>
2024-06-19 22:23:51 -0400

    Allowed laser projectiles to expire

--- weapons/LaserPistol/LaserProjectile.cs
+++ weapons/LaserPistol/LaserProjectile.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class LaserProjectile : Area3D
+public partial class LaserProjectile : RigidBody3D
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -14,6 +14,15 @@ public partial class LaserProjectile : Area3D
 
 	public override void _PhysicsProcess(double delta)
 	{
-		 
+		if(GetContactCount() > 0)
+		{
+			QueueFree();
+		}
+	}
+
+
+	private void _on_lifetime_timeout()
+	{
+		QueueFree();
 	}
 }
