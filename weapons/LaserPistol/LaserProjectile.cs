89060db6a0d8036aab38d505c13167ff7598f916
Nedim Basic <basicn86@gmail.com>
2024-06-23 05:59:14 -0400

    Added splash particles for laser

--- weapons/LaserPistol/LaserProjectile.cs
+++ weapons/LaserPistol/LaserProjectile.cs
@@ -3,6 +3,11 @@ using System;
 
 public partial class LaserProjectile : RigidBody3D
 {
+	[Export]
+	private Node3D particleSpawnPoint;
+	[Export]
+	private PackedScene particleScene;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -16,6 +21,10 @@ public partial class LaserProjectile : RigidBody3D
 	{
 		if(GetContactCount() > 0)
 		{
+			QueueFree();
+			Node3D _particles = (Node3D)particleScene.Instantiate();
+			_particles.Position = particleSpawnPoint.GlobalTransform.Origin;
+			GetParent().AddChild(_particles);
 			QueueFree();
 		}
 	}
