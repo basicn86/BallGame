ae1b175dd9a19cc703f3ca99edec1c77689cb3d7
Nedim Basic <basicn86@gmail.com>
2024-11-02 13:13:51 -0400

    Added more rotating bars

--- RotatingBar.cs
+++ RotatingBar.cs
@@ -7,10 +7,21 @@ namespace DemoLevel
 	{
 		[Export]
 		public float RotationSpeed = 1f;
+		private float _currentX = 0f;
+		private float _currentY = 0f;
+		private float _currentZ = 0f;
+
+		public override void _Ready()
+		{
+			_currentY = Rotation.Y;
+			_currentZ = Rotation.Z;
+		}
 
 		public override void _PhysicsProcess(double delta)
 		{
-			RotateX(RotationSpeed);
+			_currentX += RotationSpeed;
+
+			Rotation = new Vector3(_currentX, _currentY, _currentZ);
 		}
 	}
 }
