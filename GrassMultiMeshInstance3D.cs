35b1f778d8ff8f875d046c5041054e3512f606c6
Nedim Basic <basicn86@gmail.com>
2025-02-16 22:30:02 -0500

    Added cache to Grass multimesh

--- GrassMultiMeshInstance3D.cs
+++ GrassMultiMeshInstance3D.cs
@@ -10,6 +10,9 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 	[Export]
 	public Material Material { get; set; }
 
+	//Caching the transforms increases performance by 2x, even if it adds more operations, since we do not need to wait for the rendering server.
+	private List<Transform3D> transforms;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -18,6 +21,12 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 			Multimesh.Mesh.SurfaceSetMaterial(0, Material);
 		}
 		Multimesh.VisibleInstanceCount = Multimesh.InstanceCount;
+
+		transforms = new List<Transform3D>(Multimesh.InstanceCount);
+		for (int i = 0; i < Multimesh.InstanceCount; i++)
+		{
+			transforms.Add(Multimesh.GetInstanceTransform(i));
+		}
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,20 +34,20 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 	{
 	}
 
-    //TODO: possibly move this to its own class, may cause clutter.
-    //TODO 2: This function takes 10 ms on 30k instances. Multithreading may be needed.
-    //NOTE: This does scale with the number of instances available, as in it operates in O(n) time, where n is the number of instances.
-    public void RemoveWithinDistance(Vector3 position, float distance)
+	//TODO: possibly move this to its own class, may cause clutter.
+	//TODO 2: This function takes 10 ms on 30k instances. Multithreading may be needed.
+	//NOTE: This does scale with the number of instances available, as in it operates in O(n) time, where n is the number of instances.
+	public void RemoveWithinDistance(Vector3 position, float distance)
 	{
+		Stopwatch stopwatch = new Stopwatch();
+		stopwatch.Start();
 		int[] instanceIndexes = new int[Multimesh.VisibleInstanceCount];
 		float distanceSquared = distance * distance;
 		int instanceCount = 0;
 
 		for (int i = 0; i < Multimesh.VisibleInstanceCount; i++)
 		{
-			Transform3D instanceTransform = Multimesh.GetInstanceTransform(i);
-
-			if (instanceTransform.Origin.DistanceSquaredTo(position) < distanceSquared)
+			if (transforms[i].Origin.DistanceSquaredTo(position) < distanceSquared)
 			{
 				instanceIndexes[instanceCount] = i;
 				instanceCount++;
@@ -54,9 +63,12 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 			if (last > 0) // Ensure last is within valid range
 			{
 				Multimesh.SetInstanceTransform(instanceIndexes[i], Multimesh.GetInstanceTransform(last));
+				transforms[instanceIndexes[i]] = transforms[last];
 				last--;
 			}
 		}
 		Multimesh.VisibleInstanceCount = Multimesh.VisibleInstanceCount - instanceCount;
+		stopwatch.Stop();
+		GD.Print("RemoveWithinDistance took " + stopwatch.ElapsedMilliseconds + " ms");
 	}
 }
