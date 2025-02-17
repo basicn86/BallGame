67622013f79d51b7c7942ad337c87f0bd410a2f0
Nedim Basic <basicn86@gmail.com>
2025-02-16 22:48:02 -0500

    Tidy up

--- GrassMultiMeshInstance3D.cs
+++ GrassMultiMeshInstance3D.cs
@@ -13,7 +13,6 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 	//Caching the transforms increases performance by 2x, even if it adds more operations, since we do not need to wait for the rendering server.
 	private List<Transform3D> transforms;
 
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		for (int i = 0; i < Multimesh.InstanceCount; i++)
@@ -28,19 +27,8 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 			transforms.Add(Multimesh.GetInstanceTransform(i));
 		}
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
-	//TODO: possibly move this to its own class, may cause clutter.
-	//TODO 2: This function takes 10 ms on 30k instances. Multithreading may be needed.
-	//NOTE: This does scale with the number of instances available, as in it operates in O(n) time, where n is the number of instances.
 	public void RemoveWithinDistance(Vector3 position, float distance)
 	{
-		Stopwatch stopwatch = new Stopwatch();
-		stopwatch.Start();
 		List<int> instanceIndexes = GetInstancesToRemove(position, distance, 0, Multimesh.VisibleInstanceCount);
 
 		for (int i = 0, last = Multimesh.VisibleInstanceCount - 1; i < instanceIndexes.Count; i++)
@@ -49,7 +37,7 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 			{
 				last--;
 			}
-			if (last > 0) // Ensure last is within valid range
+			if (last > 0)
 			{
 				Multimesh.SetInstanceTransform(instanceIndexes[i], Multimesh.GetInstanceTransform(last));
 				transforms[instanceIndexes[i]] = transforms[last];
@@ -57,8 +45,6 @@ public partial class GrassMultiMeshInstance3D : MultiMeshInstance3D
 			}
 		}
 		Multimesh.VisibleInstanceCount = Multimesh.VisibleInstanceCount - instanceIndexes.Count;
-		stopwatch.Stop();
-		GD.Print("Time taken: " + stopwatch.ElapsedMilliseconds + " ms");
 	}
 
 	private List<int> GetInstancesToRemove(Vector3 position, float distance, int start, int end)
