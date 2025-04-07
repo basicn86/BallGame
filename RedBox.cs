61bc1d12787ea2f170b63ec2d7c59c3c2d69a03b
Nedim Basic <basicn86@gmail.com>
2025-04-07 04:16:39 -0400

    LockOnArea improvements

--- RedBox.cs
+++ RedBox.cs
@@ -5,12 +5,13 @@ public partial class RedBox : RigidBody3D
 {
 	[Export]
 	private Area3D PlayerDetectionArea;
-	
 
+	private Player? player;
 
 	public void _player_area_entered(Node3D body)
 	{
 		if (body is not Player) return;
+		player = body as Player;
 		PlayerDetectionArea.QueueFree();
 
 		Vector3 impulseDirection = body.GlobalPosition - GlobalPosition;
@@ -22,6 +23,7 @@ public partial class RedBox : RigidBody3D
 
 	public void _on_hurt_box_jumped_by_player()
 	{
+		player.ApplyCentralImpulse(Vector3.Up * 15f);
 		QueueFree();
 	}
 }
