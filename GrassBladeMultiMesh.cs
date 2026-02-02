bd2d1a2daefa2f4077b9cca2cbcd7da0bdd46e3e
Nedim Basic <basicn86@gmail.com>
2026-02-02 03:01:56 -0500

    Grass improvements

---
+++ GrassBladeMultiMesh.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class GrassBladeMultiMesh : MultiMeshInstance3D
+{
+	[Export]
+	public Material Material { get; set; }
+
+	public override void _Ready()
+	{
+		Multimesh.Mesh.SurfaceSetMaterial(0, Material);
+
+		for (int i = 0; i < Multimesh.InstanceCount; i++)
+		{
+			Transform3D t = Multimesh.GetInstanceTransform(i);
+
+			Basis b = new Basis(Vector3.Up, GD.Randf());
+
+			t.Basis = b;
+
+			Multimesh.SetInstanceTransform(i, t);
+		}
+	}
+}
