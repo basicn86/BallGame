39a091f69de5a26e966a92f546a81ad8d7f99c48
Nedim Basic <basicn86@gmail.com>
2026-02-15 19:15:14 -0500

    Added more coins

--- GrassBladeMultiMesh.cs
+++ GrassBladeMultiMesh.cs
@@ -9,18 +9,5 @@ public partial class GrassBladeMultiMesh : MultiMeshInstance3D
 	public override void _Ready()
 	{
 		Multimesh.Mesh.SurfaceSetMaterial(0, Material);
-
-		for (int i = 0; i < Multimesh.InstanceCount; i++)
-		{
-			Transform3D t = Multimesh.GetInstanceTransform(i);
-
-			Basis b = new Basis(Vector3.Up, GD.Randf());
-
-			b = b.Scaled(t.Basis.Scale);
-
-			t.Basis = b;
-
-			Multimesh.SetInstanceTransform(i, t);
-		}
 	}
 }
